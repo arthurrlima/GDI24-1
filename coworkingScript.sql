e4fb580ce05589552caf0c86839b9af8b2078622
arthurrlima <arl3@cin.ufpe.br>
2024-08-19 23:21:46 -0300

    update Script

--- coworkingScript.sql
+++ coworkingScript.sql
@@ -1,17 +1,18 @@
-DROP TABLE pessoa;
-DROP TABLE empresa;
 DROP TABLE tipo_espaco;
-DROP TABLE alimentacao;
-DROP TABLE sessao;
 DROP TABLE evento;
 DROP TABLE pagamento;
+DROP TABLE espaco_de_trabalho;
+DROP TABLE convidado;
+DROP TABLE organizar;
+DROP TABLE comprar;
 DROP TABLE plano_de_assinatura;
 DROP TABLE reserva;
-DROP TABLE convidado;
-DROP TABLE consumir;
+DROP TABLE sessao;
+DROP TABLE empresa;
+DROP TABLE pessoa;
 DROP TABLE usuario;
+DROP TABLE alimentacao;
 DROP TABLE endereco;
-DROP TABLE espaco_de_trabalho;
 
 CREATE TABLE endereco (
     end_CEP VARCHAR2(10),
@@ -23,16 +24,27 @@ CREATE TABLE endereco (
 );
 
 CREATE TABLE usuario (
-    id_usuario NUMBER,
+    id_usuario NUMBER DEFAULT usuario_seq.NEXTVAL,
     telefone VARCHAR2(15),
     end_CEP VARCHAR2(10),
+    cod_barras VARCHAR2(13),
 
     CONSTRAINT usuario_pk PRIMARY KEY (id_usuario),
-    CONSTRAINT usuario_endereco_fk FOREIGN KEY (end_CEP) REFERENCES endereco (end_CEP)
+    CONSTRAINT usuario_endereco_fk FOREIGN KEY (end_CEP) REFERENCES endereco (end_CEP),
+    CONSTRAINT usuario_alimentos_fk FOREIGN KEY (cod_barras) REFERENCES alimentacao (cod_barras)
+);
+
+CREATE TABLE alimentacao (
+    cod_barras VARCHAR2(13),  
+    nome VARCHAR2(100) NOT NULL,
+    id_usuario NUMBER,
+
+    CONSTRAINT alimentacao_pk PRIMARY KEY (cod_barras),
+    CONSTRAINT alimentacao_usuario_fk FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
 );
 
 CREATE TABLE pessoa (
-    id_pessoa NUMBER,
+    id_pessoa NUMBER DEFAULT usuario_seq.NEXTVAL,
     telefone VARCHAR2(15),
     cpf VARCHAR2(11) NOT NULL UNIQUE,
     end_CEP VARCHAR2(10),
@@ -43,7 +55,7 @@ CREATE TABLE pessoa (
 );
 
 CREATE TABLE empresa (
-    id_empresa NUMBER,
+    id_empresa NUMBER DEFAULT usuario_seq.NEXTVAL,
     telefone VARCHAR2(15),
     cnpj VARCHAR2(14) NOT NULL UNIQUE,
     end_CEP VARCHAR2(10),
@@ -54,7 +66,7 @@ CREATE TABLE empresa (
 );
 
 CREATE TABLE espaco_de_trabalho (
-    id_espaco NUMBER,      
+    id_espaco NUMBER DEFAULT espaco_de_trabalho_seq.NEXTVAL,      
     capacidade NUMBER NOT NULL,
 
     CONSTRAINT espaco_de_trabalho_pk PRIMARY KEY (id_espaco) 
@@ -67,19 +79,15 @@ CREATE TABLE tipo_espaco (
     CONSTRAINT espaco_trabalho_fk FOREIGN KEY (espaco_trabalho) REFERENCES espaco_de_trabalho (id_espaco)
 );
 
-CREATE TABLE alimentacao (
-    cod_barras VARCHAR2(13),  
-    nome VARCHAR2(100) NOT NULL,
-    CONSTRAINT alimentacao_pk PRIMARY KEY (cod_barras) 
-);
 
 CREATE TABLE sessao (
-    id_sessao NUMBER,
+    id_sessao NUMBER DEFAULT sessao_seq.NEXTVAL,
+    data_e_hora TIMESTAMP NOT NULL, 
     CONSTRAINT sessao_pk PRIMARY KEY (id_sessao)
 );
 
 CREATE TABLE evento (
-    id_evento NUMBER,                  
+    id_evento NUMBER DEFAULT evento_seq.NEXTVAL,                  
     data_e_hora TIMESTAMP NOT NULL,         
     duracao INTERVAL DAY TO SECOND NOT NULL, 
     qtd_pessoas NUMBER NOT NULL,    
@@ -87,7 +95,7 @@ CREATE TABLE evento (
 );
 
 CREATE TABLE pagamento (
-    id_pagamento NUMBER,                             
+    id_pagamento NUMBER DEFAULT pagamento_seq.NEXTVAL,                             
     data_e_hora TIMESTAMP NOT NULL,                    
     nome_portador_cartão VARCHAR2(100) NOT NULL,       
     cpf_portador_cartão VARCHAR2(11) NOT NULL UNIQUE,         
@@ -101,32 +109,48 @@ CREATE TABLE plano_de_assinatura (
     nivel VARCHAR2(50),  
     tempo NUMBER NOT NULL,     
     preco DECIMAL(10, 2) NOT NULL,
-    CONSTRAINT plano_de_assinatura_pk PRIMARY KEY (nivel)
+    plano_evoluido VARCHAR2(50),
+    CONSTRAINT plano_de_assinatura_pk PRIMARY KEY (nivel),
+    CONSTRAINT plano_de_assinatura_evoluir_fk FOREIGN KEY (plano_evoluido) REFERENCES plano_de_assinatura(nivel)
 );
 
 CREATE TABLE convidado (
     id_convidado NUMBER,                     
-    no_convidado NUMBER,                  
+    no_convidado NUMBER,
+    id_pessoa NUMBER,     
+    id_sessao NUMBER,             
     nome_convidado VARCHAR2(100) NOT NULL, 
     PRIMARY KEY (id_convidado, no_convidado), 
-    CONSTRAINT fk_usuario FOREIGN KEY (id_convidado) REFERENCES usuario (id_usuario)
+    CONSTRAINT fk_usuario FOREIGN KEY (id_convidado) REFERENCES usuario (id_usuario),
+    CONSTRAINT fk_convidado_pessoa FOREIGN KEY (id_pessoa) REFERENCES pessoa (id_pessoa),
+    CONSTRAINT fk_convidado_sessao FOREIGN KEY (id_sessao) REFERENCES sessao (id_sessao)
 );
 
 CREATE TABLE reserva (
     usuario_id NUMBER,                              
-    espaco_de_trabalho_id NUMBER,                   
+    espaco_de_trabalho_id NUMBER,
+    id_sessao NUMBER,                   
     PRIMARY KEY (usuario_id, espaco_de_trabalho_id),  
     CONSTRAINT reserva_usuario_fk FOREIGN KEY (usuario_id) REFERENCES usuario (id_usuario),
-    CONSTRAINT reserva_espaco_de_trabalho_fk FOREIGN KEY (espaco_de_trabalho_id) REFERENCES espaco_de_trabalho (id_espaco)
+    CONSTRAINT reserva_espaco_de_trabalho_fk FOREIGN KEY (espaco_de_trabalho_id) REFERENCES espaco_de_trabalho (id_espaco),
+    CONSTRAINT reserva_sessao_fk FOREIGN KEY (id_sessao) REFERENCES sessao (id_sessao)
 );
 
-
-CREATE TABLE consumir (
-    codigo_de_barras VARCHAR2(13),
-    usuario_id NUMBER,  
-    nome_do_produto VARCHAR2(100) NOT NULL,
-    data_e_hora_consumo TIMESTAMP NOT NULL,
-
-    CONSTRAINT consumir_pk PRIMARY KEY (codigo_de_barras),
-    CONSTRAINT consumir_usuario_fk FOREIGN KEY (usuario_id) REFERENCES usuario (id_usuario)
+CREATE TABLE organizar (
+    id_empresa INT,
+    id_evento INT,
+    PRIMARY KEY (id_empresa, id_evento),
+    FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa),
+    FOREIGN KEY (id_evento) REFERENCES evento(id)
 );
+
+CREATE TABLE comprar (
+    usuario_id NUMBER,
+    id_pagamento NUMBER,
+    nivel VARCHAR2(50),
+
+    PRIMARY KEY (usuario_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (id_pagamento) REFERENCES pagamento(id_pagamento),
+    FOREIGN KEY (nivel) REFERENCES plano_de_assinatura(nivel)
+)
