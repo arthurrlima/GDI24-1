40f9c0fb277ba8407e2a2403c621a09ed964a68c
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2024-08-21 23:42:00 -0300

    fix coworkingScript.sql

--- coworkingScript.sql
+++ coworkingScript.sql
@@ -1,18 +1,28 @@
-DROP TABLE comprar;
-DROP TABLE organizar;
-DROP TABLE reserva;
-DROP TABLE convidado;
-DROP TABLE plano_de_assinatura;
-DROP TABLE pagamento;
-DROP TABLE evento;
-DROP TABLE sessao;
-DROP TABLE tipo_espaco;
-DROP TABLE espaco_de_trabalho;
-DROP TABLE empresa;
-DROP TABLE pessoa;
-DROP TABLE alimentacao;
-DROP TABLE usuario;
 DROP TABLE endereco;
+DROP TABLE sessao;
+DROP TABLE evento;
+DROP TABLE espaco_de_trabalho;
+DROP TABLE pagamento;
+DROP TABLE usuario;
+DROP TABLE alimentacao;
+DROP TABLE pessoa;
+DROP TABLE empresa;
+DROP TABLE tipo_espaco;
+DROP TABLE plano_de_assinatura;
+DROP TABLE convidado;
+DROP TABLE reserva;
+DROP TABLE organizar;
+
+CREATE SEQUENCE sessao_seq
+    START WITH 1
+    INCREMENT BY 1
+    CACHE 20;
+
+CREATE TABLE sessao (
+    id_sessao NUMBER,
+    data_e_hora TIMESTAMP NOT NULL, 
+    CONSTRAINT sessao_pk PRIMARY KEY (id_sessao)
+);
 
 CREATE TABLE endereco (
     end_CEP VARCHAR2(10),
@@ -23,16 +33,61 @@ CREATE TABLE endereco (
     CONSTRAINT endereco_pk PRIMARY KEY (end_CEP)
 );
 
+CREATE SEQUENCE evento_seq
+    START WITH 1
+    INCREMENT BY 1
+    CACHE 20;
+
+CREATE TABLE evento (
+    id_evento NUMBER,                  
+    data_e_hora TIMESTAMP NOT NULL,         
+    duracao INTERVAL DAY TO SECOND NOT NULL, 
+    qtd_pessoas NUMBER NOT NULL,    
+    CONSTRAINT evento_pk PRIMARY KEY (id_evento)
+);
+
+CREATE SEQUENCE espaco_de_trabalho_seq
+    START WITH 1
+    INCREMENT BY 1
+    CACHE 20;
+
+CREATE TABLE espaco_de_trabalho (
+    id_espaco NUMBER,      
+    capacidade NUMBER NOT NULL,
+
+    CONSTRAINT espaco_de_trabalho_pk PRIMARY KEY (id_espaco) 
+);
+
+CREATE SEQUENCE pagamento_seq
+    START WITH 1
+    INCREMENT BY 1
+    CACHE 20;
+
+CREATE TABLE pagamento (
+    id_pagamento NUMBER,                             
+    data_e_hora TIMESTAMP NOT NULL,                    
+    nome_portador_cartao VARCHAR2(100) NOT NULL,       
+    cpf_portador_cartao VARCHAR2(11) NOT NULL UNIQUE,         
+    numero_cartao VARCHAR2(16) NOT NULL,               
+    codigo_seguranca_cartao VARCHAR2(4) NOT NULL,      
+    data_vencimento_cartao DATE NOT NULL,
+    CONSTRAINT pagamento_pk PRIMARY KEY (id_pagamento)
+);
+
+CREATE SEQUENCE usuario_seq
+    START WITH 1
+    INCREMENT BY 1
+    CACHE 20;
+
 CREATE TABLE usuario (
-    id_usuario NUMBER DEFAULT usuario_seq.NEXTVAL,
+    id_usuario NUMBER,
     telefone VARCHAR2(15),
     end_CEP VARCHAR2(10),
     cod_barras VARCHAR2(13),
     nome_usuario VARCHAR2(100),
 
     CONSTRAINT usuario_pk PRIMARY KEY (id_usuario),
-    CONSTRAINT usuario_endereco_fk FOREIGN KEY (end_CEP) REFERENCES endereco (end_CEP),
-    CONSTRAINT usuario_alimentos_fk FOREIGN KEY (cod_barras) REFERENCES alimentacao (cod_barras)
+    CONSTRAINT usuario_endereco_fk FOREIGN KEY (end_CEP) REFERENCES endereco (end_CEP)
 );
 
 CREATE TABLE alimentacao (
@@ -45,9 +100,9 @@ CREATE TABLE alimentacao (
 );
 
 CREATE TABLE pessoa (
-    id_pessoa NUMBER DEFAULT usuario_seq.NEXTVAL,
+    id_pessoa NUMBER,
     telefone VARCHAR2(15),
-    cpf VARCHAR2(11) NOT NULL UNIQUE,
+    cpf VARCHAR2(100) NOT NULL UNIQUE,
     end_CEP VARCHAR2(10),
 
     CONSTRAINT pessoa_pk PRIMARY KEY (id_pessoa),
@@ -55,10 +110,15 @@ CREATE TABLE pessoa (
     CONSTRAINT pessoa_usuario_fk FOREIGN KEY (id_pessoa) REFERENCES usuario (id_usuario)
 );
 
+CREATE SEQUENCE empresa_seq
+    START WITH 1          
+    INCREMENT BY 1        
+    CACHE 20;              
+
 CREATE TABLE empresa (
-    id_empresa NUMBER DEFAULT usuario_seq.NEXTVAL,
+    id_empresa NUMBER,
     telefone VARCHAR2(15),
-    cnpj VARCHAR2(14) NOT NULL UNIQUE,
+    cnpj VARCHAR2(100) NOT NULL UNIQUE,
     end_CEP VARCHAR2(10),
 
     CONSTRAINT empresa_pk PRIMARY KEY (id_empresa),
@@ -66,44 +126,11 @@ CREATE TABLE empresa (
     CONSTRAINT empresa_usuario_fk FOREIGN KEY (id_empresa) REFERENCES usuario (id_usuario)
 );
 
-CREATE TABLE espaco_de_trabalho (
-    id_espaco NUMBER DEFAULT espaco_de_trabalho_seq.NEXTVAL,      
-    capacidade NUMBER NOT NULL,
-
-    CONSTRAINT espaco_de_trabalho_pk PRIMARY KEY (id_espaco) 
-);
-
 CREATE TABLE tipo_espaco (
     espaco_trabalho NUMBER,            
     tipo VARCHAR2(50) NOT NULL, 
     CONSTRAINT tipo_espaco_pk PRIMARY KEY (espaco_trabalho, tipo),  
     CONSTRAINT espaco_trabalho_fk FOREIGN KEY (espaco_trabalho) REFERENCES espaco_de_trabalho (id_espaco)
-);
-
-
-CREATE TABLE sessao (
-    id_sessao NUMBER DEFAULT sessao_seq.NEXTVAL,
-    data_e_hora TIMESTAMP NOT NULL, 
-    CONSTRAINT sessao_pk PRIMARY KEY (id_sessao)
-);
-
-CREATE TABLE evento (
-    id_evento NUMBER DEFAULT evento_seq.NEXTVAL,                  
-    data_e_hora TIMESTAMP NOT NULL,         
-    duracao INTERVAL DAY TO SECOND NOT NULL, 
-    qtd_pessoas NUMBER NOT NULL,    
-    CONSTRAINT evento_pk PRIMARY KEY (id_evento)
-);
-
-CREATE TABLE pagamento (
-    id_pagamento NUMBER DEFAULT pagamento_seq.NEXTVAL,                             
-    data_e_hora TIMESTAMP NOT NULL,                    
-    nome_portador_cartão VARCHAR2(100) NOT NULL,       
-    cpf_portador_cartão VARCHAR2(11) NOT NULL UNIQUE,         
-    numero_cartão VARCHAR2(16) NOT NULL,               
-    codigo_seguranca_cartão VARCHAR2(4) NOT NULL,      
-    data_vencimento_cartão DATE NOT NULL,
-    CONSTRAINT pagamento_pk PRIMARY KEY (id_pagamento)
 );
 
 CREATE TABLE plano_de_assinatura (
@@ -115,8 +142,13 @@ CREATE TABLE plano_de_assinatura (
     CONSTRAINT plano_de_assinatura_evoluir_fk FOREIGN KEY (plano_evoluido) REFERENCES plano_de_assinatura(nivel)
 );
 
+CREATE SEQUENCE convidado_seq
+    START WITH 1          
+    INCREMENT BY 1         
+    CACHE 20;              
+
 CREATE TABLE convidado (
-    id_pessoa NUMBER DEFAULT usuario_seq.NEXTVAL,           
+    id_pessoa NUMBER,           
     no_convidado NUMBER,
     id_sessao NUMBER,             
     nome_convidado VARCHAR2(100) NOT NULL, 
@@ -130,18 +162,18 @@ CREATE TABLE reserva (
     espaco_de_trabalho_id NUMBER,
     id_sessao NUMBER,
 
-    PRIMARY KEY (usuario_id, espaco_de_trabalho_id),  
-    CONSTRAINT reserva_usuario_fk FOREIGN KEY (usuario_id) REFERENCES usuario (id_usuario),
+    PRIMARY KEY (id_usuario, espaco_de_trabalho_id),  
+    CONSTRAINT reserva_usuario_fk FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario),
     CONSTRAINT reserva_espaco_de_trabalho_fk FOREIGN KEY (espaco_de_trabalho_id) REFERENCES espaco_de_trabalho (id_espaco),
     CONSTRAINT reserva_sessao_fk FOREIGN KEY (id_sessao) REFERENCES sessao (id_sessao)
 );
 
 CREATE TABLE organizar (
-    id_empresa INT,
-    id_evento INT,
+    id_empresa NUMBER,
+    id_evento NUMBER,
     PRIMARY KEY (id_empresa, id_evento),
     FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa),
-    FOREIGN KEY (id_evento) REFERENCES evento(id)
+    FOREIGN KEY (id_evento) REFERENCES evento(id_evento)
 );
 
 CREATE TABLE comprar (
